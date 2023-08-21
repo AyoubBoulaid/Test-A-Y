0bca74be7b9b6335e9078a95a54b34380669547a
Ayoub <boulaidayoub02@gmail.com>
2023-08-21 14:33:21 -0700

    Test

--- _printf.c
+++ _printf.c
@@ -1,73 +1,66 @@
-#include <stdarg.h>
-#include <unistd.h>
+#include "main.h"
+
+void print_buffer(char buffer[], int *buff_ind);
 
 /**
- * _putchar - Writes a character to stdout.
- * @c: The character to be written.
- *
- * Return: 1 on success, -1 on error.
- */
-int _putchar(char c)
-{
-    return write(1, &c, 1);
-}
-
-/**
- * _printf - Produces output according to a format.
- * @format: Format string with conversion specifiers.
- *
- * Return: Number of characters printed.
+ * _printf - Printf function
+ * @format: format.
+ * Return: Printed chars.
  */
 int _printf(const char *format, ...)
 {
-    va_list args;
-    int i = 0, len = 0;
+	int i, printed = 0, printed_chars = 0;
+	int flags, width, precision, size, buff_ind = 0;
+	va_list list;
+	char buffer[BUFF_SIZE];
 
-    va_start(args, format);
-    if (format == NULL)
-        return -1;
+	if (format == NULL)
+		return (-1);
 
-    while (format[i] != '\0')
-    {
-        if (format[i] == '%' && format[i + 1] != '\0')
-        {
-            if (format[i + 1] == 'c')
-            {
-                char ch = va_arg(args, int);
-                _putchar(ch);
-                len++;
-                i += 2;
-                continue;
-            }
-            else if (format[i + 1] == 's')
-            {
-                char *str = va_arg(args, char *);
-                if (str == NULL)
-                    str = "(null)";
-                while (*str != '\0')
-                {
-                    _putchar(*str);
-                    len++;
-                    str++;
-                }
-                i += 2;
-                continue;
-            }
-            else if (format[i + 1] == '%')
-            {
-                _putchar('%');
-                len++;
-                i += 2;
-                continue;
-            }
-        }
-        
-        _putchar(format[i]);
-        len++;
-        i++;
-    }
-    
-    va_end(args);
-    return len;
+	va_start(list, format);
+
+	for (i = 0; format && format[i] != '\0'; i++)
+	{
+		if (format[i] != '%')
+		{
+			buffer[buff_ind++] = format[i];
+			if (buff_ind == BUFF_SIZE)
+				print_buffer(buffer, &buff_ind);
+			/* write(1, &format[i], 1);*/
+			printed_chars++;
+		}
+		else
+		{
+			print_buffer(buffer, &buff_ind);
+			flags = get_flags(format, &i);
+			width = get_width(format, &i, list);
+			precision = get_precision(format, &i, list);
+			size = get_size(format, &i);
+			++i;
+			printed = handle_print(format, &i, list, buffer,
+				flags, width, precision, size);
+			if (printed == -1)
+				return (-1);
+			printed_chars += printed;
+		}
+	}
+
+	print_buffer(buffer, &buff_ind);
+
+	va_end(list);
+
+	return (printed_chars);
 }
 
+/**
+ * print_buffer - Prints the contents of the buffer if it exist
+ * @buffer: Array of chars
+ * @buff_ind: Index at which to add next char, represents the length.
+ */
+void print_buffer(char buffer[], int *buff_ind)
+{
+	if (*buff_ind > 0)
+		write(1, &buffer[0], *buff_ind);
+
+	*buff_ind = 0;
+}
--- main.h
+++ main.h
@@ -1,7 +1,14 @@
 #ifndef MAIN_H
 #define MAIN_H
 
+#include <stdarg.h> // Include for va_list
+
+#define BUFF_SIZE 1024 // Size of the temporary buffer
+
+/* Function prototypes */
+int _printf(const char *format, ...);
 void print_buffer(char buffer[], int *buff_ind);
 
-#endif /* MAIN_H */
+/* Add any other function prototypes or definitions you might need */
 
+#endif /* MAIN_H 
